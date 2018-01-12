55ce789beed917dff4bc31d3101ca2389cea5bb7
DESKTOP-0C4HENT\WR <magewr@gmail.com>
2018-01-12 18:18:46 +0900

    AddStoryUseCase TestCase 추가

--- app/src/test/java/com/example/wr/story/interactor/AddStoryTest.java
+++ app/src/test/java/com/example/wr/story/interactor/AddStoryTest.java
@@ -1,9 +1,17 @@
 package com.example.wr.story.interactor;
 
+import com.example.wr.story.data.DataRepository;
+import com.example.wr.story.data.local.dto.StoryDTO;
+import com.example.wr.story.ui.content.TestStoryItemGenerator;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.mockito.Mockito.verify;
 
 /**
  * Created by WR on 2018-01-12.
@@ -11,13 +19,21 @@ import org.mockito.junit.MockitoJUnitRunner;
 
 @RunWith(MockitoJUnitRunner.class)
 public class AddStoryTest {
+
+    private AddStory addStory;
+    @Mock DataRepository dataRepository;
+
     @Before
     public void setup() {
-
+        MockitoAnnotations.initMocks(this);
+        addStory = new AddStory(dataRepository);
     }
 
     @Test
     public void addTest() {
-
+        StoryDTO item = new TestStoryItemGenerator().generateStoryDTO();
+        addStory.buildUseCaseCompletable(item);
+        verify(dataRepository).addStoryDto(item);
     }
+
 }
