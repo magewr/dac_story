f2f192bc21ebd198c0c899bb2c1693030e7725d0
DESKTOP-0C4HENT\WR <magewr@gmail.com>
2018-01-12 18:32:52 +0900

    DetailPresenter Test 주석 업데이트

--- app/src/test/java/com/example/wr/story/ui/content/detail/DetailPresenterTest.java
+++ app/src/test/java/com/example/wr/story/ui/content/detail/DetailPresenterTest.java
@@ -18,6 +18,8 @@ import org.mockito.junit.MockitoJUnitRunner;
 import java.util.ArrayList;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -47,6 +49,7 @@ public class DetailPresenterTest {
 
     @Test
     public void addPictureOnViewModeTest() {
+        //보기 모드에서는 사진 추가가 호출되어도 무시되어야 한다.
         detailPresenter.currentDisplayMode = DisplayMode.ViewMode;
         ArrayList<String> imagePath = new ArrayList<>();
         imagePath.add("test1");
@@ -57,6 +60,7 @@ public class DetailPresenterTest {
 
     @Test
     public void updateOnViewModeTest() {
+        //보기 모드에서는 업데이트 요청이 들어와도 무시되어야 한다.
         detailPresenter.currentDisplayMode = DisplayMode.ViewMode;
         ArrayList<String> imagePath = new ArrayList<>();
         imagePath.add("test1");
@@ -68,28 +72,32 @@ public class DetailPresenterTest {
 
     @Test
     public void updateWithNoPictureItem() {
+        //사진이 없는 상태에서 업데이트가 들어오면 에러가 리턴되어야 한다.
         StoryDTO item = new TestStoryItemGenerator().generateStoryDTO();
         ArrayList<String> imagePath = new ArrayList<>();
         item.setImagePathList(imagePath);
         detailPresenter.updateStory(item, presenterResultListener);
-        verify(updateStory, never()).execute(any(SimpleDisposableCompletableObserver.class), any(StoryDTO.class));
+        verify(presenterResultListener).onResult(eq(false), anyString());
     }
 
     @Test
     public void calculateIndicatorTest() {
+        //전체 이미지 5개, 인덱스 0일 경우에 인디케이터는 1, 5가 되어야 한다.
         when(adapterModel.getImageCount()).thenReturn(5);
         detailPresenter.calculatePageIndicatorIndex(0);
         verify(view).setViewPagerIndicatorText(1, 5);
+        //전체 이미지 5개, 인덱스 5(사진추가 아이템)일 경우에 인디케이터는 5, 5가 되어야 한다.
         detailPresenter.calculatePageIndicatorIndex(5);
         verify(view).setViewPagerIndicatorText(5,5);
     }
 
     @Test
     public void onBackPressedHandleTest() {
+        //보기 모드에서는 BackPressed가 발생하면 Alert이 뜨지 않아야 한다.
         detailPresenter.currentDisplayMode = DisplayMode.ViewMode;
         detailPresenter.handleOnBackPressed();
         verify(view, never()).showCancelEditingAlertDialog();
-
+        //편집 모드에서는 BackPressed가 발생하면 Alert이 발생해야 한다.
         detailPresenter.currentDisplayMode = DisplayMode.EditMode;
         detailPresenter.handleOnBackPressed();
         verify(view).showCancelEditingAlertDialog();
