79805066d51944f86344cff47a817102f5c1d982
DESKTOP-0C4HENT\WR <magewr@gmail.com>
2018-01-12 11:15:26 +0900

    테스트 파일 정리

---
+++ app/src/test/java/com/example/wr/story/interactor/AddStoryTest.java
@@ -0,0 +1,23 @@
+package com.example.wr.story.interactor;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+/**
+ * Created by WR on 2018-01-12.
+ */
+
+@RunWith(MockitoJUnitRunner.class)
+public class AddStoryTest {
+    @Before
+    public void setup() {
+
+    }
+
+    @Test
+    public void addTest() {
+
+    }
+}
